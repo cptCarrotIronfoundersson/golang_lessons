7ad5e688521eb4da4110ea5ddfa1ab02b5f7a6bb
mihailpuhov <mikhail.pukhov@instamart.ru>
2022-04-01 17:30:25 +0300

    Fix by comments

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/fs"
 	"os"
 	"strings"
 )
@@ -15,6 +16,47 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func getFileData(filePath string) (string, fs.FileInfo, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", nil, err
+	}
+
+	defer func() {
+		err = file.Close()
+	}()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return "", nil, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	var fileContent string
+
+	for scanner.Scan() {
+		fileContent = scanner.Text()
+		break
+	}
+	return fileContent, fileInfo, err
+}
+
+func FillEnv(myEnvs Environment) Environment {
+	for _, item := range os.Environ() {
+		splits := strings.Split(item, "=")
+		key := splits[0]
+
+		myEnvs[key] = EnvValue{
+			Value:      splits[1],
+			NeedRemove: false,
+		}
+	}
+	return myEnvs
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
@@ -22,26 +64,11 @@ func ReadDir(dir string) (Environment, error) {
 	if err != nil {
 		return nil, err
 	}
-	environment := make(Environment)
+	environment := FillEnv(make(Environment))
 	for _, d := range dirList {
 		if !d.IsDir() {
 			filepath := fmt.Sprintf("%s/%s", dir, d.Name())
-			file, err := os.Open(filepath)
-			fileInfo, _ := file.Stat()
-			if err != nil {
-				return nil, err
-			}
-			defer file.Close()
-
-			scanner := bufio.NewScanner(file)
-			// optionally, resize scanner's capacity for lines over 64K, see next example
-			var fileContent string
-
-			for scanner.Scan() {
-				fileContent = scanner.Text()
-				break
-			}
-
+			fileContent, fileInfo, err := getFileData(filepath)
 			if err != nil {
 				return nil, err
 			}
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -5,42 +5,49 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	for key, value := range env {
+	myEnv := env
+	for key, value := range myEnv {
 		if value.NeedRemove {
+			delete(myEnv, key)
 			err := os.Unsetenv(key)
 			if err != nil {
 				fmt.Println(err)
-				return 1
 			}
 			continue
 		} else if strings.Contains(key, "=") {
 			fmt.Println("Знак = запрещен для обозначения переменной окружения")
 			continue
 		}
-		err := os.Unsetenv(key)
-		if err != nil {
-			fmt.Println(err)
-			return 1
-		}
-		err = os.Setenv(key, value.Value)
-		if err != nil {
-			fmt.Println(err)
-			return 1
-		}
 	}
 	args := cmd[1:]
 	command := cmd[0]
-	if len(cmd) > 0 {
-		command := exec.Command(command, args...)
+	envs := os.Environ()
 
-		out, err := command.Output()
+	for key, value := range myEnv {
+		envs = append(envs, fmt.Sprintf("%s=%s", key, value.Value))
+	}
+	Cmd := &exec.Cmd{
+		Path: command,
+		Args: append([]string{command}, args...),
+		Env:  envs,
+	}
+	if filepath.Base(command) == command {
+		if lp, err := exec.LookPath(command); err == nil {
+			Cmd.Path = lp
+		} else {
+			return 1
+		}
+	}
+	if len(cmd) > 0 {
+		out, err := Cmd.Output()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println(err, "Ошибка при выполнении cmd.Output()")
 			return 1
 		}
 		fmt.Println(string(out))
