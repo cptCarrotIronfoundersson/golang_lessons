8ae70b1f61811a969741764b77893e63a9d2f9d0
mihailpuhov <mikhail.pukhov@instamart.ru>
2022-04-09 09:23:45 +0300

    initial commit

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
-type TelnetClient interface {
-	Connect() error
-	io.Closer
-	Send() error
-	Receive() error
+type TelnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) *TelnetClient {
+	return &TelnetClient{address: address, timeout: timeout, in: in, out: out}
+}
+
+func (c *TelnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return fmt.Errorf("dial: %w", err)
+	}
+	c.conn = conn
 	return nil
+}
+
+func (c *TelnetClient) Close() error {
+	return c.conn.Close()
+}
+
+func (c *TelnetClient) Send() error {
+	_, err := io.Copy(c.conn, c.in)
+	return err
+}
+
+func (c *TelnetClient) Receive() error {
+	_, err := io.Copy(c.out, c.conn)
+	return err
 }
 
 // Place your code here.
